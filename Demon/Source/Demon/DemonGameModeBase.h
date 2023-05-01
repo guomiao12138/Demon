6b8d0b2f95a2b4dc97da12cd785b18a5655a605e
guomiao <>
2023-05-01 19:00:17 +0800

    no message

---
+++ Demon/Source/Demon/DemonGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DemonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEMON_API ADemonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
